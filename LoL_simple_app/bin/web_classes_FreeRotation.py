00d4a93da604405cacd2f37534eafb881cae5eaf
Nate <Nate>
2016-11-12 17:45:23 -0600

    added images for the free rotation champs

--- LoL_simple_app/bin/web_classes_FreeRotation.py
+++ LoL_simple_app/bin/web_classes_FreeRotation.py
@@ -19,8 +19,11 @@ class FreeRotation:
 		data = rq.get(rac.champion_live_data(key))
 		data_raw = data.json()
 
+		image_url = 'http://ddragon.leagueoflegends.com/cdn/img/champion/loading/'
+
 		free_list = fcr.get_free_list(data_raw)
 		free_champs = fcr.free_champs(free_list)
+		free_image_list = free_champs
 
 		return render.free_champions_POST(free_one=free_champs[0],
 											free_two=free_champs[1],
@@ -31,4 +34,14 @@ class FreeRotation:
 											free_seven=free_champs[6],
 											free_eight=free_champs[7],
 											free_nine=free_champs[8],
-											free_ten=free_champs[9])
+											free_ten=free_champs[9],
+											image_one=image_url+free_image_list[0]+'_0.jpg',
+											image_two=image_url+free_image_list[1]+'_0.jpg',
+											image_three=image_url+free_image_list[2]+'_0.jpg',
+											image_four=image_url+free_image_list[3]+'_0.jpg',
+											image_five=image_url+free_image_list[4]+'_0.jpg',
+											image_six=image_url+free_image_list[5]+'_0.jpg',
+											image_seven=image_url+free_image_list[6]+'_0.jpg',
+											image_eight=image_url+free_image_list[7]+'_0.jpg',
+											image_nine=image_url+free_image_list[8]+'_0.jpg',
+											image_ten=image_url+free_image_list[9]+'_0.jpg')
